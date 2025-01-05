5d6f0a55b9f4ca98bdd410b4dc92ac1ffd5feb7e
Nicolaas @ Tappy @ Sunny Side Up <github@sunnysideup.co.nz>
2025-01-05 14:34:49 +1300

    FIX: basic fixes

--- src/FuzzyPartialMatchFilter.php
+++ src/FuzzyPartialMatchFilter.php
@@ -17,58 +17,58 @@ class FuzzyPartialMatchFilter extends SearchFilter
         return ['not'];
     }
 
-    /**
-     * Apply the match filter to the given variable value
-     *
-     * @param string $value The raw value
-     * @return string
-     */
-    protected function getMatchPattern($value)
-    {
-        return "%$value%";
-    }
+    // /**
+    //  * Apply the match filter to the given variable value
+    //  *
+    //  * @param string $value The raw value
+    //  * @return string
+    //  */
+    // protected function getMatchPattern($value)
+    // {
+    //     return "%$value%";
+    // }
 
-    public function matches(mixed $objectValue): bool
-    {
-        $isCaseSensitive = $this->getCaseSensitive();
-        if ($isCaseSensitive === null) {
-            $isCaseSensitive = $this->getCaseSensitiveByCollation();
-        }
-        $caseSensitive = $isCaseSensitive ? '' : 'i';
-        $negated = in_array('not', $this->getModifiers());
-        $objectValueString = (string) $objectValue;
+    // public function matches(mixed $objectValue): bool
+    // {
+    //     $isCaseSensitive = $this->getCaseSensitive();
+    //     if ($isCaseSensitive === null) {
+    //         $isCaseSensitive = $this->getCaseSensitiveByCollation();
+    //     }
+    //     $caseSensitive = $isCaseSensitive ? '' : 'i';
+    //     $negated = in_array('not', $this->getModifiers());
+    //     $objectValueString = (string) $objectValue;
 
-        // can't just cast to array, because that will convert null into an empty array
-        $filterValues = $this->getValue();
-        if (!is_array($filterValues)) {
-            $filterValues = [$filterValues];
-        }
+    //     // can't just cast to array, because that will convert null into an empty array
+    //     $filterValues = $this->getValue();
+    //     if (!is_array($filterValues)) {
+    //         $filterValues = [$filterValues];
+    //     }
 
-        // This is essentially a in_array($objectValue, $filterValues) check, with some special handling.
-        $hasMatch = false;
-        foreach ($filterValues as $filterValue) {
-            if (is_bool($objectValue)) {
-                // A partial boolean match should match truthy and falsy values.
-                $doesMatch = $objectValue == $filterValue;
-            } else {
-                $filterValue = (string) $filterValue;
-                $regexSafeFilterValue = preg_quote($filterValue, '/');
-                $doesMatch = preg_match('/' . $regexSafeFilterValue  . '/u' . $caseSensitive, $objectValueString);
-            }
-            // Any match is a match
-            if ($doesMatch) {
-                $hasMatch = true;
-                break;
-            }
-        }
+    //     // This is essentially a in_array($objectValue, $filterValues) check, with some special handling.
+    //     $hasMatch = false;
+    //     foreach ($filterValues as $filterValue) {
+    //         if (is_bool($objectValue)) {
+    //             // A partial boolean match should match truthy and falsy values.
+    //             $doesMatch = $objectValue == $filterValue;
+    //         } else {
+    //             $filterValue = (string) $filterValue;
+    //             $regexSafeFilterValue = preg_quote($filterValue, '/');
+    //             $doesMatch = preg_match('/' . $regexSafeFilterValue  . '/u' . $caseSensitive, $objectValueString);
+    //         }
+    //         // Any match is a match
+    //         if ($doesMatch) {
+    //             $hasMatch = true;
+    //             break;
+    //         }
+    //     }
 
-        // Respect "not" modifier.
-        if ($negated) {
-            $hasMatch = !$hasMatch;
-        }
+    //     // Respect "not" modifier.
+    //     if ($negated) {
+    //         $hasMatch = !$hasMatch;
+    //     }
 
-        return $hasMatch;
-    }
+    //     return $hasMatch;
+    // }
 
     protected function applyOne(DataQuery $query)
     {
